cb463a226d9f5b82c2a85e34522f0e00fc070063
Blinkuu <40406905+Blinkuu@users.noreply.github.com>
2018-11-12 23:00:54 +0100

    Add files via upload

--- Lab04/PSO_Clerc.py
+++ Lab04/PSO_Clerc.py
@@ -18,8 +18,8 @@ class Particle:
         self.v_x = np.random.uniform(-0.01, 0.01)
         self.v_y = np.random.uniform(-0.01, 0.01)
         self.v_max = v_max
-        self.c1 = 2.0
-        self.c2 = 2.0
+        self.c1 = 2
+        self.c2 = 2
 
     def fitness(self):
         return self.x ** 2 + self.y ** 2 - 20 * (math.cos(math.pi * self.x) + math.cos(math.pi * self.y) - 2)
@@ -27,8 +27,9 @@ class Particle:
     def update_velocity(self, p_best_x, p_best_y, g_best_x, g_best_y):
         U1 = U(0, self.c1)
         U2 = U(0, self.c2)
+        W = omega(self.c1, self.c2)
         offset_x = U1 * (p_best_x - self.x) + U2 * (g_best_x - self.x)
-        velocity_x = self.v_x + offset_x
+        velocity_x = W * (self.v_x + offset_x)
         if velocity_x > self.v_max:
             self.v_x = self.v_max
         elif velocity_x < (-1) * self.v_max:
@@ -37,7 +38,7 @@ class Particle:
             self.v_x = velocity_x
 
         offset_y = U1 * (p_best_y - self.y) + U2 * (g_best_y - self.y)
-        velocity_y = self.v_y + offset_y
+        velocity_y = W * (self.v_y + offset_y)
         if velocity_y > self.v_max:
             self.v_y = self.v_max
         elif velocity_y < (-1) * self.v_max:
@@ -53,14 +54,14 @@ class Particle:
         self.y = self.y + self.v_y
 
         if self.x > x_max:
-            offset = self.x - 10.0
-            self.x = abs(x_max) - offset
+            offset = abs(self.x) - 10.0
+            self.x = x_max - offset
         if self.x < (-1) * x_max:
             offset = abs(self.x) - 10.0
             self.x = (-1) * x_max + offset
         if self.y > y_max:
-            offset = self.y - 10.0
-            self.y = abs(y_max) - offset
+            offset = abs(self.y) - 10.0
+            self.y = y_max - offset
         if self.y < (-1) * y_max:
             offset = abs(self.y) - 10.0
             self.y = (-1) * y_max + offset
@@ -96,6 +97,13 @@ def U(x, y):
     return rnd.uniform(x, y)
 
 
+def omega(c1, c2):
+    #fi = c1 + c2
+    #w = 2 / (2 - fi - math.sqrt((fi ** 2) - (4 * fi)))
+    #return w
+    return 0.7298
+
+
 def search_best_index(fit_tab):
     index = 0
     for i in range(len(fit_tab)):
@@ -107,14 +115,18 @@ def search_best_index(fit_tab):
 def plot(population, pop_size):
     x = []
     y = []
+
+    plt.axis([-10, 10, -10, 10])
+
     for i in range(pop_size):
         x.append(population.pop_tab[i].x)
         y.append(population.pop_tab[i].y)
     plt.scatter(x, y)
-    plt.axis([-10, 10, -10, 10])
+
     plt.show()
 
 
+# Algorithm
 pop_size = 20
 v_max = 0.01
 
@@ -125,7 +137,7 @@ g_best_particle = [population.pop_tab[g_best_index].x, population.pop_tab[g_best
 g_best_value = population.pop_tab[g_best_index].fitness()
 
 k = 0
-while (k < 2000):
+while k < 2000:
 
     if k % 100 == 0:
         plot(population, pop_size)
@@ -136,18 +148,18 @@ while (k < 2000):
     p_best_particle = [population.pop_tab[p_best_index].x, population.pop_tab[p_best_index].y]
     p_best_value = population.pop_tab[p_best_index].fitness()
 
-    if p_best_value < g_best_value:
-        g_best_index = p_best_index
-        g_best_particle = p_best_particle
-        g_best_value = p_best_value
-
     for i in range(pop_size):
         population.pop_tab[i].update_velocity(population.pop_tab[p_best_index].x, population.pop_tab[p_best_index].y,
                                               population.pop_tab[g_best_index].x, population.pop_tab[g_best_index].y)
         population.pop_tab[i].update_position()
 
-    print("VELOCITY = ", k, format(population.pop_tab[0].v_x, '.10f'), format(population.pop_tab[0].v_y, '.10f'))
+    if p_best_value < g_best_value:
+        g_best_index = p_best_index
+        g_best_particle = p_best_particle
+        g_best_value = p_best_value
+
     #print("BEST PARTICLE = ", g_best_particle, " FITNESS = ", g_best_value)
+    print("VELOCITY = ", k, population.pop_tab[0].v_x, population.pop_tab[0].v_x)
 
     k += 1
 
